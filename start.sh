872d36068b82d8615898b08214510cb97c761330
Daniel Akio Teixeira <danielakioteixeira@gmail.com>
2020-06-29 14:05:50 -0300

    Adicionando nome ao container

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 app_name='jeitto'
 docker build -t ${app_name} .
-docker run --env-file=.env -d -p 5000:5000 ${app_name}:latest
+docker run --env-file=.env --name jeitto--backend-challenge -d -p 5000:5000 ${app_name}:latest
